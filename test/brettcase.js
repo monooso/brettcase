d72b2afd18dafd8a4402d61e32352cda3ae9e900
Stephen Lewis <stephen@experiencehq.net>
2016-01-30 19:15:23 +0000

    Fix linter warnings in test file.

--- test/brettcase.js
+++ test/brettcase.js
@@ -1,24 +1,26 @@
-var nock = require('nock')
-  , should = require('should')
-  , brettcase = require('../lib/brettcase')
-  , baseUrl = 'http://brettterpstra.com/titlecase';
+'use strict';
 
-function thenWhenCatchWasExpected(data) {
+require('should');
+var nock = require('nock');
+var brettcase = require('../lib/brettcase');
+var baseUrl = 'http://brettterpstra.com/titlecase';
+
+function thenWhenCatchWasExpected(/* data */) {
   throw Error('promise should return an error');
 }
 
 function mockSuccessfulCall(input, output) {
   return nock(baseUrl)
     .get('/')
-    .query({title: input})
-    .reply(200, output, {'Content-Length': output.length});
+    .query({ title: input })
+    .reply(200, output, { 'Content-Length': output.length });
 }
 
 describe('#brettcase', function () {
   it('constructs the query string', function () {
-    var input = 'input'
-      , output = 'output'
-      , mockApi = mockSuccessfulCall(input, output);
+    var input = 'input';
+    var output = 'output';
+    var mockApi = mockSuccessfulCall(input, output);
 
     return brettcase(input).then(function () {
       mockApi.done();
@@ -26,9 +28,9 @@ describe('#brettcase', function () {
   });
 
   it('returns the api response body', function () {
-    var input = 'input'
-      , output = 'output'
-      , mockApi = mockSuccessfulCall(input, output);
+    var input = 'input';
+    var output = 'output';
+    var mockApi = mockSuccessfulCall(input, output);
 
     return brettcase(input).then(function (data) {
       data.should.be.exactly(output);
@@ -37,11 +39,11 @@ describe('#brettcase', function () {
   });
 
   it('throws an error if the request fails', function () {
-    var input = 'input'
-      , output = 'Epic fail'
-      , mockApi = nock(baseUrl).get('/').query({title: input}).replyWithError(output);
+    var input = 'input';
+    var output = 'Epic fail';
+    var mockApi = nock(baseUrl).get('/').query({ title: input }).replyWithError(output);
 
-    var catchPromise = function(error) {
+    var catchPromise = function (/* error */) {
       mockApi.done();
     };
 
@@ -50,10 +52,10 @@ describe('#brettcase', function () {
 
   // The API returns a 200 code, even if there is a 5xx error.
   it('throws an error if the api does not return a string', function () {
-    var input = 'input'
-      , mockApi = mockSuccessfulCall(input, '');  // Empty response.
+    var input = 'input';
+    var mockApi = mockSuccessfulCall(input, '');  // Empty response.
 
-    var catchPromise = function(error) {
+    var catchPromise = function (/* error */) {
       mockApi.done();
     };
 
@@ -62,8 +64,8 @@ describe('#brettcase', function () {
 
   // Integration test, to confirm the API still behaves as expected.
   it('works with the real api', function () {
-    var input = 'of mice and men'
-      , output = 'Of Mice and Men';
+    var input = 'of mice and men';
+    var output = 'Of Mice and Men';
 
     brettcase(input).then(function (data) {
       data.should.be.exactly(output);
